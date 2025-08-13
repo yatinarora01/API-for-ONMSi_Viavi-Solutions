bfd7b4bfe9eee31561093bdb6007231c031f6311
Yatin Arora <yatin.arora@viavisolutions.com>
2025-08-13 14:54:23 +0530

    Add root URL route

--- onmsi_facade_v5.py
+++ onmsi_facade_v5.py
@@ -1,3 +1,4 @@
+# file: onmsi_facade_v5.py
 import requests
 import xmltodict
 import json
@@ -20,10 +21,6 @@ def fetch_detail(url):
         return r.json()
     except ValueError:
         return xmltodict.parse(r.content)
-
-@app.route('/', methods=["GET"])
-def home():
-    return "Welcome to the ONMSi Fiber Remote Testing Solution!"
 
 @app.route("/rs/otus/full", methods=["GET"])
 def otus_full():
